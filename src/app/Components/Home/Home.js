755e47beec57de9fb995074c54b6a6a5a92a2b2d
Muhammad Saad Kayani <iamsaadk69@gmail.com>
2025-07-19 15:24:11 +0500

    why choose done

--- src/app/Components/Home/Home.js
+++ src/app/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './Hero/HeroSection'
+import WhyChoose from './WhyChooseUs/WhyChoose'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <HeroSection />
+      <WhyChoose />
     </div>
   )
 }
---
+++ src/app/Components/Home/WhyChooseUs/WhyChoose.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import WhyChooseCard from './WhyChooseCard'
+
+const WhyChoose = () => {
+    return (
+        <div className='pb-16 pt-16'>
+            <h1 className='mt-6 text-2xl md:text-3xl capitalize text-center font-bold'>
+                Why Choose This Application</h1>
+            <div className='mt-20 grid w-[90%] mx-auto grid-cols-1 md:grid-cols-2
+                            lg:grid-cols-3 xl:grid-cols-4 gap-12'>
+            <div>
+                <WhyChooseCard 
+                image="/images/i1.png"
+                title= "Create Free Account"
+                linktext="Start Earning"
+                />
+            </div>
+            <div>
+                <WhyChooseCard 
+                image="/images/i2.png"
+                title= "Create Free Account"
+                linktext="Start Earning"
+                />
+            </div>
+            <div>
+                <WhyChooseCard 
+                image="/images/i3.png"
+                title= "Create Free Account"
+                linktext="Start Earning"
+                />
+            </div>
+            <div>
+                <WhyChooseCard 
+                image="/images/i4.png"
+                title= "Create Free Account"
+                linktext="Start Earning"
+                />
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default WhyChoose
